951fff16fc4f1e5011b88fc3a8fb57f242256cb4
Seohyun Jung <jsh318900@gmail.com>
2017-09-16 15:15:51 -0400

    Created AutoModule and listed TODO lists to work on

--- build-pb.ps1
+++ build-pb.ps1
@@ -9,4 +9,5 @@ New-Item -Force -ItemType directory -Path pb
   --java_out=glennifer/robot_state/src/main/java/. `
   --java_out=glennifer/HCI/src/main/java/. `
   --java_out=glennifer/autodrill/src/main/java/. `
+  --java_out=glennifer/automodule/src/main/java/. `
   messages.proto
---
+++ glennifer/automodule/src/main/java/cwrubotix/automodule/AutoModule.java
@@ -0,0 +1,41 @@
+package cwrubotix.automodule;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import com.rabbitmq.client.AMQP;
+
+import com.cwrubotix.glennifer.Messages;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 
+ * Autonomous Module which controls robot's movements through mining cycles.
+ * AutoModule is a state function which switches back and forth between Transit state,
+ * Digging state, Dumping state. AutoModule should be able to decide robot's current/next actions,
+ * depending on which state it is in.
+ * 
+ * @author Seohyun Jung
+ *
+ */
+public class AutoModule{
+	private Stage currentStage;
+	private enum Stage {TRANSIT, DIGGING, DUMPING, EMERGENCY};
+	
+	/*
+	 *TODO list
+	 *	1) Decide on course of actions on possible situations (Obstacles, Path plan, Setting up Dumping position)
+	 *	2) Create appropriate Consumers, Messages system for decision making messages.
+	 *	3) Subscribe for sensor values or modules needed.
+	 *	4) Come up with Error Handling mechanism.
+	 *	5) Set up Connection Factory.
+	 */
+	
+}
