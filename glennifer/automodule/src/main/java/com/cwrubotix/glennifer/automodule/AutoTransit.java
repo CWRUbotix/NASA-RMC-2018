9f718806850a20876e55abe7a24607fa95721ae2
Seohyun Jung <jsh318900@gmail.com>
2017-10-21 13:15:21 -0400

    Set April tag as origin

--- glennifer/automodule/src/main/java/com/cwrubotix/glennifer/automodule/AutoTransit.java
+++ glennifer/automodule/src/main/java/com/cwrubotix/glennifer/automodule/AutoTransit.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeoutException;
 
 
 public class AutoTransit{
-	private final Position DUMP_BIN = new Position(1.89F, 0.0F, Math.PI, 0.0F);
+	private final Position DUMP_BIN = new Position(0.0F, 0.0F, Math.PI, 0.0F);
 	/*Horizontal line representing where digging arena starts.*/
 	private final Position DIGGING_AREA = new Position(0.0F, 4.41F, -1.0, 0.0F);
 	private final float CLEARANCE_DIST = 0.3F; //Setting this to 30cm for now. Will have to change it after testing locomotion.
