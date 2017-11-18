f356d760ab952daef2efa99634783a5f56a1ce84
Robbie Dozier <importantigrav@gmail.com>
2017-11-18 02:15:07 -0500

    Fleshed out PathFinder more, deleted PathFinderDijkstra

--- glennifer/automodule/src/main/java/com/cwrubotix/glennifer/automodule/PathFinder.java
+++ glennifer/automodule/src/main/java/com/cwrubotix/glennifer/automodule/PathFinder.java
@@ -16,17 +16,38 @@ import java.util.ArrayList;
 public class PathFinder<T extends PathFindingAlgorithm> {
     private T pathFindingAlgorithm;
     private ArrayList<Position> obstacles;
+    private Path path;
+    private Position currentPos;
+    private Position startPos;
+    private Position targetPos;
 
-    public PathFinder() {
+    public PathFinder(T pathFindingAlgorithm, Position startPos, Position targetPos) {
         obstacles = new ArrayList<Position>();
+        this.pathFindingAlgorithm = pathFindingAlgorithm;
+        this.startPos = startPos;
+        this.targetPos = targetPos;
+        path = pathFindingAlgorithm.computePath(this.startPos, this.targetPos);
+        this.currentPos = path.getStart();
     }
 
     public Path getPath() {
+        return path;
+    }
 
+    public Position getCurrentPos() {
+        return currentPos;
+    }
+
+    public Position getStartPos() {
+        return startPos;
+    }
+
+    public Position getTargetPos() {
+        return targetPos;
     }
 
     public void registerObstacle(Position obstacle) {
         obstacles.add(obstacle);
-        // pathFindingAlgorithm.computePath();
+        path = pathFindingAlgorithm.computePath(currentPos, targetPos);
     }
 }
