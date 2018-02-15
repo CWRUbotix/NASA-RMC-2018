0e4957ee7db47a2a54d331effbb6c2c124f4701c
jsh318900 <jsh318900@gmail.com>
2018-02-15 09:34:32 -0500

    Commit most recent changes bc Adam's is without a personal computer as of late

--- glennifer/HCIuino/HCIuino.ino
+++ glennifer/HCIuino/HCIuino.ino
@@ -1,11 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////////
+//
 //	PREPROCESSOR INCLUDES
+//
 ////////////////////////////////////////////////////////////////////////////////
 //#include <ODriveArduino.h>
 #include <math.h>
 
 ////////////////////////////////////////////////////////////////////////////////
+//
 //	PREPROCESSOR DEFINES
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 //Command type codes
@@ -15,12 +19,15 @@
 #define RPY_HCI_TEST 			(0xA5)
 
 // Sizes & Number of things
+#define SENSORE_DATA_SIZE 		(1)
+#define MOTOR_ID_SIZE 			(1)
 #define CMD_HEADER_SIZE			(2)
 #define RPY_HEADER_SIZE			(2)
-#define HEADER_FIELDS			(3)
-#define INSTRUCTION_LEN 		(3)
+//#define HEADER_FIELDS			(3)
+//#define INSTRUCTION_LEN 		(3)
 #define DEFAULT_BUF_LEN 		(256)
 #define SENSORE_ID_SIZE 		(1)
+#define MOTOR_ID_SIZE 			(1)
 #define UART_BAUD 				(115200)
 
 // FAULT CODES
@@ -47,19 +54,22 @@
 
 //SENSOR STUFF
 enum SensorHardware {
-  SH_NONE,
-//  SH_RC_POT,
-//  SH_RC_ENC,
-//  SH_RC_CUR,
-  SH_I2C_BAT,
-  SH_PIN_LIMIT,
-  SH_PIN_POT
+	SH_NONE,
+	SH_BL_POT,		// BL := brushless motor
+	SH_BL_ENC,		//
+	SH_BL_CUR,		//
+	SH_BR_POT,		// BR := brushed motor
+	SH_BR_ENC,		//
+	SH_BR_CUR,		//
+	SH_I2C_BAT,		//
+	SH_PIN_LIMIT,	//
+	SH_PIN_POT		//
 };
 
 //SENSOR INFO
 typedef struct SensorInfo{
 	SensorHardware hardware;
-	uint8_t addr; 			// When hardware = SH_RC_*
+	uint8_t addr; 			// When hardware = SH_I2C_* or ...
 	uint8_t whichMotor; 	// When hardware = SH_RC_*
 	uint8_t whichPin; 		// When hardware = SH_PIN_*
 	bool is_reversed;		// When hardware = SH_PIN_LIMIT
@@ -69,23 +79,23 @@ typedef struct SensorInfo{
 
 //MOTOR STUFF
 enum MotorHardware {
-  MH_NONE,
-  MH_RC_PWM,
-  MH_RC_VEL,
-  MH_RC_POS,
-  MH_RC_POS_BOTH,
-  MH_ST_PWM,
-  MH_ST_POS,
-  MH_ST_PWM_BOTH,
-  MH_PIN_PWM,
-  MH_ALL
+	MH_NONE,
+	MH_BL_PWM, 		// BL := brushless
+	MH_BL_VEL,		//
+	MH_BL_POS,		//
+	MH_BL_POS_BOTH,	//
+	MH_BR_PWM, 		// BR := brushed
+	MH_BR_POS,		//
+	MH_BR_PWM_BOTH,	//
+	MH_PIN_PWM,		//
+	MH_ALL			//
 };
 
 //MOTOR INFO
 typedef struct MotorInfo{
-	MotorHardware hardware;
-	uint8_t addr;
-	uint8_t whichMotor;
+	MotorHardware hardware = MH_NONE; // default is NONE
+	uint8_t addr; 			// 
+	uint8_t whichMotor;		// 
 	uint16_t scale; 		// 1 unless needed
 	uint16_t setPt; 		// set point for motor (rather that use an array)
 	float kp; 				// When hardware = MH_RC_POS or MC_RC_VEL
@@ -98,6 +108,8 @@ typedef struct MotorInfo{
 	uint32_t accel;
 	uint16_t feedbackSensorID;
 	float saturation;
+	int16_t lastUpdateTime;	// replaces the motor_lastUpdateTime array
+	float integral; 		// replaces the motor_integrals array
 }MotorInfo;
 
 
@@ -119,8 +131,7 @@ uint8_t sensor_lastLimitVals[DEFAULT_BUF_LEN]	= {}; 	// All initialized to 0
 int16_t sensor_storedVals[DEFAULT_BUF_LEN] 		= {}; 	// All initialized to 0
 float motor_integrals[DEFAULT_BUF_LEN] 			= {}; 	//All initialized to 0
 int16_t motor_lastUpdateTime[DEFAULT_BUF_LEN] 	= {}; 	//All initialized to 0
-bool stopped 									= true;
-
+bool stopped 									= true;	// default status is stopped
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -161,11 +172,12 @@ FAULT_T popLastFault(){
 
 ////////////////////////////////////////////////////////////////////////////////
 void setup_sensors(){
+	// FOREACH SENSOR
 
 }
 ////////////////////////////////////////////////////////////////////////////////
 void setup_motors(){
-	
+	// FOREACH MOTOR
 }
 ////////////////////////////////////////////////////////////////////////////////
 void setup_comms() {
@@ -235,11 +247,12 @@ uint8_t cmd_sense_num_sensors(byte* cmd){
 ////////////////////////////////////////////////////////////////////////////////
 FAULT_T hciAnswer(byte* cmd, byte* rpy){
 	//byte rpy[DEFAULT_BUF_LEN];
-	bool success = false;
-	uint8_t size = RPY_HEADER_SIZE;
-	uint8_t retval;
+	bool success 	= false;
+	uint8_t size 	= RPY_HEADER_SIZE;
+	uint8_t bodyLen = 0;
+	uint8_t retval 	= 0;
+	FAULT_T fault 	= FAULT_FAILED_WRITE;
 
-	FAULT_T fault;
 	uint8_t type = cmd_type(cmd);
 	rpy_init(rpy, type);
 	uint8_t num = 0;
@@ -247,7 +260,7 @@ FAULT_T hciAnswer(byte* cmd, byte* rpy){
 	switch(type){
 		case CMD_HCI_TEST:
 			rpy[RPY_HEADER_SIZE] = RPY_HCI_TEST; 	// data goes immediately after header
-			size++;
+			bodyLen++;
 			break;
 		case CMD_READ_SENSORS:
 			int16_t value;
@@ -258,7 +271,7 @@ FAULT_T hciAnswer(byte* cmd, byte* rpy){
 				rpy[(3*i)+RPY_HEADER_SIZE]   = ID;
 				rpy[(3*i)+RPY_HEADER_SIZE+1] = (uint8_t)(value >> 8);
 				rpy[(3*i)+RPY_HEADER_SIZE+2] = (uint8_t)(value & 0xFF);
-				size += 3;
+				bodyLen += 3;
 			}
 			break;
 		case CMD_SET_OUTPUTS:
@@ -270,22 +283,24 @@ FAULT_T hciAnswer(byte* cmd, byte* rpy){
 				rpy[RPY_HEADER_SIZE+i*3]   = ID;
 				rpy[RPY_HEADER_SIZE+i*3+1] = (uint8_t)(setPt >> 8);
 				rpy[RPY_HEADER_SIZE+i*3+2] = (uint8_t)(setPt & 0xFF);
-				size+=3;
+				bodyLen+=3;
 			}
 			break;
 
 	}
 
-	rpy_set_len(rpy, size);
-	
-
-	SerialUSB.write(rpy, size);
-
+	rpy_set_len(rpy, bodyLen); 	// we only want to consider body length
+	size 	+= bodyLen;
+	retval 	= SerialUSB.write(rpy, size);
+	success = (retval == size);
 	if(success){
-		clear_fault_log();
-		return NO_FAULT;
+		//clear_fault_log();
+		fault = NO_FAULT;
+	}else{
+		fault = FAULT_FAILED_WRITE;
 	}
-	return FAULT_FAILED_WRITE;
+
+	return fault;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -293,27 +308,32 @@ FAULT_T hciAnswer(byte* cmd, byte* rpy){
 //		• write these bytes to the client
 ////////////////////////////////////////////////////////////////////////////////
 FAULT_T hciWrite(byte rpy[]){
-	uint8_t retval;
-	uint16_t len = rpy_len(rpy);	// DIFF from last year
-	retval = SerialUSB.write(rpy,len+2);
-	if (retval != len+2) {
-    return FAULT_FAILED_WRITE;
-  }
-  return NO_FAULT;
+	uint16_t len 	= rpy_len(rpy);	// DIFF from last year
+	fault FAULT_T 	= FAULT_FAILED_WRITE;
+	uint8_t retval 	= SerialUSB.write(rpy,len+2);
+	
+	if (retval == len+2) {
+    	fault = NO_FAULT;
+	}else{
+		fault = FAULT_FAILED_WRITE;
+	}
+
+	return fault;
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
-uint16_t rpy_len(byte * rpy){
-	uint16_t ret;
-	ret += rpy[0];
-	ret << 8;
-	ret += rpy[1];
-	return ret;
+uint8_t rpy_len(byte * rpy){
+	// uint16_t ret;
+	// ret += rpy[0];
+	// ret << 8;
+	// ret += rpy[1];
+	return rpy[1];
 }
 ////////////////////////////////////////////////////////////////////////////////
 void rpy_init(byte* rpy, uint8_t type){
-	rpy[0] = type;
+	rpy[0] = type; 	// first byte is type
+	rpy[1] = 0; 	// explicitly initialize to 0
 }
 ////////////////////////////////////////////////////////////////////////////////
 void rpy_set_len(byte* rpy, uint8_t length){
@@ -322,17 +342,21 @@ void rpy_set_len(byte* rpy, uint8_t length){
 ////////////////////////////////////////////////////////////////////////////////
 uint8_t cmd_sense_sensor_id(byte* cmd, uint8_t i){
 	uint8_t index = CMD_HEADER_SIZE; 	// index of the first byte of the body
-	index += (i * SENSORE_ID_SIZE); 	// index to access the i-th sensor
+	index += (i * SENSOR_DATA_SIZE); 	// index of the i-th sensor
 	return cmd[index]; 					// return the id
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// 	@param ID:	takes a sensor ID
+// 	@param val:	ptr to the 16-bit signed int where the sensor data will go
 ////////////////////////////////////////////////////////////////////////////////
 FAULT_T read_sensor(uint8_t ID, int16_t* val){
-	SensorInfo sensor_info = sensor_infos[ID];
-	uint8_t status;
-	bool valid;
-	int32_t val32;
-	int16_t dummy;
-	int16_t readVal;
+	SensorInfo sensor_info 	= sensor_infos[ID];
+	uint8_t status 			= 0;
+	bool valid 				= false;
+	int32_t val32 			= 0;
+	int16_t dummy 			= 0;
+	int16_t readVal 		= 0;
 
 	switch (sensor_info.hardware) {
 		case SH_PIN_LIMIT:
@@ -348,9 +372,7 @@ FAULT_T read_sensor(uint8_t ID, int16_t* val){
 			sensor_storedVals[ID] 	= (sensor_storedVals[ID] * (1 - sensor_info.responsiveness)) + (readVal * sensor_info.responsiveness);
 			*val 					= sensor_storedVals[ID];
 			break;
-
 	}
-
 	return NO_FAULT;
 }
 
@@ -362,7 +384,6 @@ FAULT_T read_sensor(uint8_t ID, int16_t* val){
 //		act on the command (cmd)
 //
 //	for each motor, maintain it's status
-//
 ////////////////////////////////////////////////////////////////////////////////
 void maintain_motors(byte* cmd, bool success){
 	// if(stopped){
@@ -379,22 +400,21 @@ void maintain_motors(byte* cmd, bool success){
 
 	// update motor outputs
 	uint8_t i 		= 0;
-	MotorInfo current_motor;// = NULL;
-//	while( (current_motor = motor_infos[i]) != NULL){
-//
-//		// do motor maintaining things
-//
-//		i++;
-//	
-//	}
-  for(i=0; i<DEFAULT_BUF_LEN; i++){
-    current_motor = motor_infos[i];
-    // do motor things
-  }
+	MotorInfo motor;// = NULL;
+
+	for(i=0; i<DEFAULT_BUF_LEN; i++){
+		motor = motor_infos[i];
+
+		if(motor.hardware == MotorHardware.MH_NONE){
+			continue;
+		}
+		// do motor maintainance things
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // analagous to "execute(cmd)"
+// not sure this is necessary
 ////////////////////////////////////////////////////////////////////////////////
 FAULT_T update_motor_infos(byte* cmd){
 	return NO_FAULT;
@@ -402,6 +422,7 @@ FAULT_T update_motor_infos(byte* cmd){
 
 ////////////////////////////////////////////////////////////////////////////////
 // update sensor data requested by client
+// not sure this is necessary
 ////////////////////////////////////////////////////////////////////////////////
 FAULT_T update_sensor_infos(byte* cmd){
 	return NO_FAULT;
@@ -449,43 +470,3 @@ void loop(){
 		fault_code = hciAnswer(cmd, rpy);	// reply to the client
 	}
 }
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-//
-
-//
-////////////////////////////////////////////////////////////////////////////////
-// void update_state(byte* cmd){
-// 	uint8_t type 	= cmd_type(cmd);
-// 	uint8_t num_sensors_requested;
-// 	uint8_t num_motors_requested;
-// 	FAULT_T retfault;
-
-// 	rpy_init(rpy, type);
-
-// 	switch(type){
-// 		case CMD_HCI_TEST:
-// 			rpy_set_len(rpy, RPY_HEADER_SIZE + 1);
-// 			rpy[RPY_HEADER_SIZE] = RPY_HCI_TEST;
-// 			break;
-
-// 		case CMD_READ_SENSORS:
-// 			num_sensors_requested = cmd_sense_num_sensors(cmd);
-// 			rpy_set_len(rpy, num_sensors_requested);
-// 			for(uint8_t i = 0; i < num_sensors_requested; i++){
-// 				uint8_t id = cmd_sense_sensor_id(cmd, i);
-// 				int16_t val;
-// 				retfault = read_sensor(id, &val);
-// 			}
-// 			break;
-
-// 		case CMD_SET_OUTPUTS:
-
-// 			break;
-// 	}
-// }
-
