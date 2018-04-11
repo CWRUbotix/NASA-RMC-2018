ddb1738a2e334e2d6aacb8ccaaa2c59918e159ba
Steven Leonis <Spwizzard2@gmail.com>
2018-04-11 17:02:19 -0400

    runglenn.sh changes'

--- runglenn.sh
+++ runglenn.sh
@@ -6,9 +6,9 @@ datestring=$(date +%Y_%m_%d_%H_%M_%S)
 #cd /home/ubuntu/workspace/NASA-RMC-2017/
 
 # start the things
-cd glennifer/HCI
-nohup /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp target/hci-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.hci.ModuleMain #&> "/home/ubuntu/logs/$datestring/ModuleMainOutput.log" &
-cd ../..
+#cd glennifer/HCI
+#nohup /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp target/hci-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.hci.ModuleMain #&> "/home/ubuntu/logs/$datestring/ModuleMainOutput.log" &
+#cd ../..
 
 cd glennifer/robot_state
 nohup /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp target/robot_state-1.0-SNAPSHOT.jar com.cwrubotix.glennifer.robot_state.StateModule #&> "/home/ubuntu/logs/$datestring/StateModuleOutput.log" &
